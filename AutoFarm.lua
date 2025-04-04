e4224302029b5282832da35f0a464709f34321a3
Distortion <sebastian99812@outlook.com>
2025-04-03 22:04:38 -0500

    Update AutoFarm.lua

--- AutoFarm.lua
+++ AutoFarm.lua
@@ -24,8 +24,8 @@ if game.PlaceId ~= 91694942823334 then
 end
 
 task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DistortionAltFR/sans.rng/refs/heads/main/Antilag.lua "))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DistortionAltFR/sans.rng/refs/heads/main/antiafk.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DistortionAltFR/sans.rng/refs/heads/main/Antilag.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DistortionAltFR/sans.rng/refs/heads/main/Antiafk.lua"))()
     print("External scripts loaded successfully.")
 end)
 
