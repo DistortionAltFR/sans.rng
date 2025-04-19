85508dd5aa21949ad5ca4687cfee922620bd7af8
Distortion <sebastian99812@outlook.com>
2025-04-19 12:39:01 -0500

    Update AUTOTI.lua

--- AUTOTI.lua
+++ AUTOTI.lua
@@ -2,9 +2,9 @@ local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")  
 local RunService = game:GetService("RunService")  
 local StarterGui = game:GetService("StarterGui")
+local antikick = loadstring(game:HttpGet("https://raw.githubusercontent.com/DistortionAltFR/sans.rng/refs/heads/main/Antikick.lua"))()
 local antiafk = loadstring(game:HttpGet("https://raw.githubusercontent.com/DistortionAltFR/sans.rng/refs/heads/main/Antiafk.lua"))() 
 local antilag = loadstring(game:HttpGet("https://raw.githubusercontent.com/DistortionAltFR/sans.rng/refs/heads/main/Antilag.lua"))()
-local antikick = loadstring(game:HttpGet("https://raw.githubusercontent.com/DistortionAltFR/sans.rng/refs/heads/main/Antikick.lua"))()
 local TARGET_PLACE_IDS = {  
     87841196505389,
     99198920478418, 
