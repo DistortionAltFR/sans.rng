af8ed85c3cf0a61fce0eb75dadf773fcbed2ffbe
Distortion <sebastian99812@outlook.com>
2026-02-11 16:20:11 -0500

    Simplify script loader by removing print statements
    Removed loading message for Aimbot and success confirmation.

---
+++ sobre.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DistortionAltFR/sans.rng/refs/heads/main/aimbot.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DistortionAltFR/sans.rng/refs/heads/main/rngidkk.lua"))()
